a79aae48c7cc4878b1b3805fdbc2af96702bb534
AyaneHana <130580666+AyaneHana@users.noreply.github.com>
2023-06-07 23:17:05 +0800

    Update cbproxy.sh

--- cbproxy.sh
+++ cbproxy.sh
@@ -1,5 +1,5 @@
 ACTUAL="maro"
-read -s "Password: " enteredpass
+read -s -p "Password: " enteredpass
 echo ""
 
 [ "$enteredpass" != "$ACTUAL" ] && echo "Sorry" && echo "exiting" || pkg install openssl curl figlet -y && wget https://raw.githubusercontent.com/AyaneHana/CBProxy/main/install.sh && bash install.sh
