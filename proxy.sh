d7196e3717971e3827b08fb057793b5d64e4053c
AyaneHana <130580666+AyaneHana@users.noreply.github.com>
2023-12-02 02:22:24 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,5 +1,4 @@
 ACTUAL="4.43 resell=ban"
 read -s -p "Password: " enteredpass
 echo ""
-rm install.sh
 [ "$enteredpass" != "$ACTUAL" ] && echo "Sorry" && echo "exiting" || pkg install openssl curl figlet -y && wget https://raw.githubusercontent.com/AyaneHana/CBProxy/main/install.sh && bash install.sh
